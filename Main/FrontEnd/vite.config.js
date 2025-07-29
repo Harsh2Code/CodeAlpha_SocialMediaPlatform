e3f2059bcb875f70aa108fa7bae77b6db8c3050e
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-30 01:33:12 +0530

    added API fallback because can't reach other pages

--- Main/FrontEnd/vite.config.js
+++ Main/FrontEnd/vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  server: {
+    historyApiFallback: true,
   },
 })
