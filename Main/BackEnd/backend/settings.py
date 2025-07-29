310e7a31f999c87599083ea24df3b9a5cf4e1545
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-29 22:51:19 +0530

    FrontEnd is configured for frontend development

--- Main/BackEnd/backend/settings.py
+++ Main/BackEnd/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-g3^(nt8_1&!r=!0lmi@)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'https://socialmedia-backend-ipwx.onrender.com,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'socialmedia-backend-ipwx.onrender.com,localhost').split(',')
 
 
 # Application definition
