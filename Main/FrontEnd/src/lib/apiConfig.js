b539d0e84f05768a3fa656449c0e7854aac1eb75
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-30 09:30:00 +0530

    Frontend trying to comm W locoalhost fix1

--- Main/FrontEnd/src/lib/apiConfig.js
+++ Main/FrontEnd/src/lib/apiConfig.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://social-media-platform-social-media-platform.j.aivencloud.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default API_BASE_URL;
