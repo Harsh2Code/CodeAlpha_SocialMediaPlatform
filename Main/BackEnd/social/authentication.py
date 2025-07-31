1819a4c974a7e26a0fa78b617d3fea2d725209bf
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-31 12:40:18 +0530

    auth correct attempt 1

--- Main/BackEnd/social/authentication.py
+++ Main/BackEnd/social/authentication.py
@@ -10,11 +10,10 @@ class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         if email is None or password is None:
             return None
-        try:
-            user = UserModel.objects.get(email=email)
-        except UserModel.DoesNotExist:
+        users = UserModel.objects.filter(email=email)
+        if not users.exists():
             return None
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
+        user = users.first()
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
         return None
