593a8417c3adc495fd0d24b0d85584b8b7c8a13b
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-31 18:22:09 +0530

    att 12

--- Main/BackEnd/backend/settings.py
+++ Main/BackEnd/backend/settings.py
@@ -36,8 +36,28 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'socialmedia-backend-ipwx.onrend
 # Add localhost:5173 and 127.0.0.1:5173 to allowed hosts for local frontend dev
 ALLOWED_HOSTS += ['localhost', 'localhost:5173', '127.0.0.1', '127.0.0.1:5173']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://socialmedia-frontend-1tqk.onrender.com",
+]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'POST',
+    'PUT',
+]
 
 # Application definition
 
