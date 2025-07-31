a98163c650c5e0377cd194e658605eb80700c707
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-07-31 16:07:30 +0530

    cors blockin fix att 3

--- Main/BackEnd/backend/urls.py
+++ Main/BackEnd/backend/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from social.views_custom_auth import CustomObtainAuthToken
 
 
 urlpatterns = [
-    path('api-token-auth/', CustomObtainAuthToken.as_view(), name='api_token_auth'),
     path('', lambda request: redirect('/api/posts/')),
     path('admin/', admin.site.urls),
     path('api/', include('social.urls')),
