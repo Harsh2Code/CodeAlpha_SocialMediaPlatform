052e4d9452c7b62c9a7d0b22bae4c7a339e48cc9
Harsh2Code <harsh8522119@jmieti.edu.in>
2025-08-02 18:08:34 +0530

    fix: profile detail att2

--- Main/BackEnd/social/models.py
+++ Main/BackEnd/social/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     profile_picture = models.URLField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    nationality = models.CharField(max_length=100, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.username
